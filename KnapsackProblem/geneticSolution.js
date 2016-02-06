6a80347e9131b92ee816f28f0c8a0db8dcbbe2ef
DaichiSaito <dchan1230@gmail.com>
2016-02-06 19:34:09 +0900

    repair coment

--- KnapsackProblem/geneticSolution.js
+++ KnapsackProblem/geneticSolution.js
@@ -1,8 +1,18 @@
 // 
 // ナップサック問題を遺伝的アルゴリズムを用いて解く
-// author: Diamond Script
+// author: D. Saito
+//
+// 品物の数 items
+// 品物の価値 value
+// 品物の重み cost
+// バックの容量 totalCapacity
+// 
+// 遺伝子の設定
+// 0なら品物を入れない
+// 1なら品物を入れる
 //
 
+// 評価値関数
 function fitness(gene, costsOfItems, valuesOfItems, totalCapacity) {
   var sumOfCosts = 0;
   var sumOfValues = 0;
@@ -15,10 +25,11 @@ function fitness(gene, costsOfItems, valuesOfItems, totalCapacity) {
   return (sumOfCosts < totalCapacity) ? sumOfValues : 0;
 }
 
-const ITEMS = 20;
+var ITEMS = 20;
 var costsOfItems = new Array(ITEMS);
 var valuesOfItems = new Array(ITEMS);
 var totalCapacity = 30;
+var elite = null;
 // とりあえずランダムに作ります
 for(var i=0;i<ITEMS;i++){
   costsOfItems[i] = Math.floor(Math.random()*8)+1;//ランダム
